155f2b60143d177f32d2e7eb1da363ed252fc2ef
davep3rrett <david.m.perrett@gmail.com>
2019-07-05 16:34:46 -0400

    Implement delete-rows fn

--- examples/cd-database/cd-database.lisp
+++ examples/cd-database/cd-database.lisp
@@ -67,3 +67,7 @@
 	       (if rating (setf (getf row :rating) rating))
 	       (if ripped-p (setf (getf row :ripped) ripped)))
 	     row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*))))
+
