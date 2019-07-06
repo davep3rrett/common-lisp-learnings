89c6ebd2c75b708cdc3167d51923ff048b9a218d
davep3rrett <david.m.perrett@gmail.com>
2019-07-05 23:32:25 -0400

    Reimplement `where` as a macro
    and fix mismatched parens in fn delete-rows

--- examples/cd-database/cd-database.lisp
+++ examples/cd-database/cd-database.lisp
@@ -41,13 +41,13 @@
 (defun select (selector-fn)
   (remove-if-not selector-fn *db*))
 
-(defun where (&key title artist rating (ripped nil ripped-p))
-  #'(lambda (cd)
-      (and
-       (if title (equal (getf cd :title) title) t)
-       (if artist (equal (getf cd :artist) artist) t)
-       (if rating (equal (getf cd :rating) rating) t)
-       (if ripped-p (equal (getf cd :ripped) ripped) t))))
+; (defun where (&key title artist rating (ripped nil ripped-p))
+;   #'(lambda (cd)
+;       (and
+;        (if title (equal (getf cd :title) title) t)
+;        (if artist (equal (getf cd :artist) artist) t)
+;        (if rating (equal (getf cd :rating) rating) t)
+;        (if ripped-p (equal (getf cd :ripped) ripped) t))))
 
 (defun update (selector-fn &key title artist rating (ripped nil ripped-p))
   (setf *db*
@@ -61,5 +61,14 @@
 	     row) *db*)))
 
 (defun delete-rows (selector-fn)
-  (setf *db* (remove-if selector-fn *db*))))
+  (setf *db* (remove-if selector-fn *db*)))
 
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+     collecting (make-comparison-expr (pop fields) (pop fields))))
+
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
